427b22270cef008958c04d905f7bcdd64ed0a8e3
Nicolas <nicolas.droesch@gmail.com>
2024-12-04 15:02:09 +0100

    Update et passage en windowsForm avec pattern CleanArchitecture

---
+++ App/domain/entities/article.cs
@@ -0,0 +1,45 @@
+namespace domain.entities 
+{
+    public class Article
+    {
+        public string nom { get; set; }
+        public decimal prix { get; set; }
+        public int quantite { get; set; }
+
+        public Article(string nom, decimal prix, int quantite)
+        {
+            this.nom = nom;
+            this.prix = prix;
+            this.quantite = quantite;
+        }
+
+        //public void Afficher()
+        //{
+        //    Console.WriteLine($"Article : {nom}, Prix : {prix}, Quantité : {quantite}");
+        //}
+
+        public void Ajouter(int quantity)
+        {
+            if (quantity > 0)
+            {
+                quantite += quantity;
+            }
+            else
+            {
+                throw new System.Exception("La quantité à ajouter doit être un nombre entier positif.");
+            }
+        }
+
+        public void Retirer(int quantity)
+        {
+            if (quantity > 0 && quantity <= quantite)
+            {
+                quantite -= quantity;
+            }
+            else
+            {
+                throw new System.Exception("Quantité invalide.");
+            }
+        }
+    }
+}
